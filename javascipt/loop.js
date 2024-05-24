e92cb8873519f6e64c3a0369a38066bde0067a69
joshua <joshuapplg@gmail.com>
2024-05-24 13:58:16 +0700

    array loop

--- javascipt/loop.js
+++ javascipt/loop.js
@@ -48,3 +48,17 @@ while (i <= 100){
     console.log(i);
     i++;
 }
+
+
+// Buatlah sebuah variabel dengan nama evenNumber yang merupakan sebuah array
+let evenNumber = [];
+
+// Menggunakan for loop untuk mengisi bilangan genap pada array
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 === 0) {
+    evenNumber.push(i);
+  }
+}
+
+// Output array evenNumber
+console.log(evenNumber);
