092b038e337aa9d40603bccd435918fe112e347a
joshua <joshuapplg@gmail.com>
2024-05-24 13:52:09 +0700

    object project

---
+++ javascipt/belajarobject.js
@@ -0,0 +1,24 @@
+// const restaurant ={
+//    name: "bakso mangdicoding",
+//    city: "Bandung",
+//    drink: "esteh",
+//    food: "Bakso",
+//    isVegan: false
+// }
+
+// const name = restaurant;
+// const favoriteDrink = "favorite drink";
+
+// console.log(restaurant, favoriteDrink)
+
+const restaurant = {
+    name: "Bakso Mang Dicoding",
+    city: "Bandung",
+    "favorite drink": "Es Teh",
+    "favorite food": "Bakso",
+    isVegan: false
+};
+
+const name = restaurant.name;
+
+const favoriteDrink = restaurant["favorite drink"];
