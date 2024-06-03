10cc2b0fe69bae262e6b962d92d24b6b25b3ff5a
joshua <joshuapplg@gmail.com>
2024-06-03 13:53:52 +0700

    tambahan

---
+++ javascipt/functional/index.js
@@ -0,0 +1,9 @@
+const names = ['joshua', 'sasuke', 'jeff', 'gismo']
+
+const newNamesWithExmark = []
+
+for(let i = 0 ; i<names.length; i++){
+    newNamesWithExmark.push(`${names[i]}`)
+}
+
+console.log(newNamesWithExmark)
