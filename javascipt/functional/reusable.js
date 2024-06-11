812bc38e891bd7b31e02ecb348273d9abdaa65b4
joshua <joshuapplg@gmail.com>
2024-06-11 13:55:17 +0700

    joshua

--- javascipt/functional/reusable.js
+++ javascipt/functional/reusable.js
@@ -1,4 +1,21 @@
+
+
 const newArray = ['Joshua', 'imanuel', 'jeff', 'yasus'].map((name)=> {return  `${name}!`})
 console.log(newArray)
 const truthyArray = [1, '', 'Hallo', 0, null, 'Harry', 14].filter((item) => Boolean(item));
 console.log(truthyArray)
+
+// Perhatikan kode berikut:
+
+class MyCustomError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "MyError";
+    }
+}
+
+try {
+    throw new MyCustomError("This is an error");
+} catch (e) {
+    console.log(e.message);
+}
